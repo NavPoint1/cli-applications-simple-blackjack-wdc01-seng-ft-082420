3520316458b582337a707d66383adf3d7c30a56a
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 02:04:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,10 +63,10 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  until card_total > 21 {
+  until card_total > 21 do
     card_total = hit?(card_total)
     display_card_total(card_total)
-  }
+  end
 end_game
 end
     
