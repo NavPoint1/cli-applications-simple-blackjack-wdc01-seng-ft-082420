2ca5e96327b5a00e90f13a26acdd4b9395b56313
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 02:04:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,6 +61,12 @@ end
 
 def runner
   # code runner here
-  
+  welcome
+  card_total = initial_round
+  until card_total > 21 {
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  }
+end_game
 end
     
