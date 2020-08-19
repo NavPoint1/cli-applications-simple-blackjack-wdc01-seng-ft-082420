354ba641be557773c8e55493b8e931582b5df0f7
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 01:48:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,12 +10,12 @@ end
 
 def display_card_total(card_total)
   # code #display_card_total here
-  "Your cards add up to #{card_total}"
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
-  "Type 'h' to hit or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -25,7 +25,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
